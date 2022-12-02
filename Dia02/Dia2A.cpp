bcb8e365677fbb540b4f13a4e1ca97c02fe28667
Nico Cossio <nicocossiom@gmail.com>
2022-12-02 20:43:06 +0100

    parte 1

--- Dia02/Dia2A.cpp
+++ Dia02/Dia2A.cpp
@@ -4,50 +4,40 @@
 // https://opensource.org/licenses/MIT
 // import priority_queue
 
-#include <iostream>
-#include <queue>
 #include <string>
 #include <fstream>
 #include <aocutils.h>
 
 using namespace std;
-int main() {
-    string line;
-    int max_cal = 0;
-    
-    fstream input = get_input_stream(1, ChallengePart::A);
-    int current_cal = 0;
 
-    while (getline(input, line)) {
-        if (line.empty()) {
-            if (current_cal > max_cal) {
-                max_cal = current_cal;
-            }
-            current_cal = 0;
-        } else {
-            int cal_item = stoi(line);
-            current_cal += cal_item;
-        }
-    }
-    cout << max_cal << endl;
-    return 0;
+using RPS = enum class RPS{ ROCK = 1, PAPER = 2, SCISSORS = 3};
+using RPSRound =  struct { RPS opponent; RPS me; };
+template <typename E>
+constexpr typename std::underlying_type<E>::type to_underlying(E e) noexcept {
+    return static_cast<typename std::underlying_type<E>::type>(e);
 }
 
-// int main2() {
-//     priority_queue<int> cal_q;
-//     string line;
-//     int total_cal = 0;
-//     freopen("input1", "r", stdin);
-//     cout << "Reading from file" << endl;
-//     while (getline(cin, line)) {
-//         if (line.empty()) {
-//             cal_q.push(total_cal);
-//             total_cal = 0;
-//         } else {
-//             int current_cal = stoi(line);
-//             total_cal += current_cal;
-//         }
-//     }
-//     cout << cal_q.top() << endl;
-//     return 0;
-// }
+RPS translate_char(char c) {
+    if (c == 'A' || c == 'X') return RPS::ROCK;
+    else if (c == 'B' || c == 'Y') return RPS::PAPER;
+    else return RPS::SCISSORS; // C or Z
+}
+RPSRound translate_input(string input_string){
+    RPSRound round = {translate_char(input_string[0]), translate_char(input_string[2])};
+    return round;
+}
+
+int main() {
+    fstream input = get_input_stream(2, ChallengePart::A);
+    string line;
+    int score = 0;
+    while(getline(input, line)) {
+        RPSRound round = transalate_input(line);
+        int fight = to_underlying(round.me) - to_underlying(round.opponent);
+        if(fight == 0) score+=3; // same -> tie
+        else if(fight == 1 || fight == -2) score += 6; // ROCK vs PAPER, PAPER VS SCISSORS, SCISSORS VS ROCK
+        score += to_underlying(round.me);
+    }
+    cout << score << endl;
+    return 0;
+}
